8ea6edc7b631b24b6300a51c53fb2790862d7a18
Stijn De Pestel <stijn.depestel@intec.ugent.be>
2016-12-07 20:07:33 +0100

    Bumped version number for http-interceptor.

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('google@1.1.15', ['client', 'server']);
-  api.use(['stijndepestel:http-interceptor@0.5.0'], ['server']);
+  api.use(['stijndepestel:http-interceptor@0.6.0'], ['server']);
   api.use(['meteorhacks:picker@1.0.3'], ['server']);
   api.add_files('oauth-fake-client.js', 'client');
   api.add_files('google-fake.js', [ 'server' ]);
