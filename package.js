18aca382e6249a01bb2e26dc56a600a2a78f6cfe
Stijn De Pestel <stijn.depestel@intec.ugent.be>
2016-12-09 11:10:05 +0100

    Bumped version of stijndepestel:http-interceptor.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: "stijndepestel:google-fake",
   summary: "A fake for use in testing. Fakes the oauth calls amongst other APIs.",
-  version: "0.0.6",
+  version: "0.0.7",
   git: "https://github.com/stijndepestel/meteor-google-fake",
   debugOnly: true
 });
 
 Package.on_use(function (api) {
   api.use('google@1.1.15', ['client', 'server']);
-  api.use(['stijndepestel:http-interceptor@0.6.0'], ['server']);
+  api.use(['stijndepestel:http-interceptor@0.7.0'], ['server']);
   api.use(['meteorhacks:picker@1.0.3'], ['server']);
   api.add_files('oauth-fake-client.js', 'client');
   api.add_files('google-fake.js', [ 'server' ]);
